0e5622ba93b20a7f1cf23a87c02d772edfd844d0
icamacho80 <ingridcamacho@yahoo.com>
2025-02-25 19:07:29 -0500

    jupyter coding file

---
+++ Resources/housing_price.sql
@@ -0,0 +1,17 @@
+Create table housing_price(
+brokered_by VARCHAR,
+status VARCHAR,
+price VARCHAR,
+bed VARCHAR,
+bath VARCHAR,
+acre_lot VARCHAR,
+street VARCHAR,    
+city VARCHAR,
+state VARCHAR,
+zip_code VARCHAR,
+house_size VARCHAR,
+prev_sold_date VARCHAR 
+);
+
+select * from housing_price
+
